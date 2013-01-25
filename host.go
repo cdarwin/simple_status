81d365760abdca2765cd8f7b4b770bfba4e8e5af
Jason Thigpen <jasont@skyboximaging.com>
2013-01-25 09:37:28 -0800

    New system/host endpoint returns only hostname
    New host handler method

--- host.go
+++ host.go
@@ -5,6 +5,14 @@ import (
 	"os"
 )
 
+func hostHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := json.MarshalIndent(host(), "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(auth(b, r.FormValue("token")))
+}
+
 func host() string {
 	host, err := os.Hostname()
 	if err != nil {
