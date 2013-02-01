b245754b8394fcad1e28a02fae72edb403c0e383
Jason Thigpen <darwin@senet.us>
2013-01-31 18:23:57 -0800

    New makeHandler and doMarshall methods
    makeHandler takes care of all the auth before reaching other methods
    doMarshall does all marshalling for the handlers

--- handlers.go
+++ handlers.go
@@ -6,22 +6,32 @@ import (
 	"net/http"
 )
 
-func diskHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		log.Println(err)
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var s string
+		if err := r.ParseForm(); err != nil {
+			log.Println(err)
+		}
+		if r.FormValue("token") != *token {
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+		switch r.FormValue("disk") {
+		case "":
+			s = "/"
+		default:
+			s = r.FormValue("disk")
+		}
+		fn(w, r, s)
 	}
-	var D DiskStatus
-	if r.FormValue("disk") == "" {
-		D = DiskUsage("/")
-	} else {
-		D = DiskUsage(r.FormValue("disk"))
-	}
-	m := DiskStatus{D.All, D.Used, D.Free}
+}
+
+func doMarshall(m interface{}) []byte {
 	b, err := json.MarshalIndent(m, "", "  ")
 	if err != nil {
 		log.Println(err)
 	}
-	w.Write(auth(b, r.FormValue("token")))
+	return b
 }
 
 func hostHandler(w http.ResponseWriter, r *http.Request) {
